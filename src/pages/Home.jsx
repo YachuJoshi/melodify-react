0907b588c5fdde68e39f28a01c61a3c6dfac16da
Yachu Joshi <yachujoshi1234@gmail.com>
2020-05-10 14:40:43 +0545

    Header Section Added :sparkles:

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
 
-const Home = (props) => {
+import Logo from '../img/melodify-logo.png';
+
+import styles from '../sass/home.module.scss';
+
+const Home = () => {
   return (
     <>
-      <h1>Hello World!</h1>
+      <header className={styles.header}>
+
+        <div className={styles.navigation__bar}>
+          <figure className={styles.logo}>
+            <img src={Logo} alt="Logo" className={styles.logo__image} />
+            <figcaption className={styles.logo__name}>Melodify</figcaption>
+          </figure>
+          <nav className={styles.navigation}>
+            <ul className={styles.navigation__list}>
+              <li className={styles.navigation__item}>Premium</li>
+              <li className={styles.navigation__item}>Help</li>
+              <li className={styles.navigation__item}>Download</li>
+              <li className={styles.navigation__item}>Sign Up</li>
+              <li className={styles.navigation__item}>Login</li>
+            </ul>
+          </nav>
+        </div>
+
+        <div className={`${styles.mainWrapper}`}>
+          <h1 className={styles.headerText}>Your Very Own <br />Personal Music Companion</h1>
+          <button className={styles.buttonFree}>Get Melodify Free</button>
+          <button className={styles.buttonPremium}>Get Melodify Premium</button>
+        </div>
+      </header>
     </>
   );
 };

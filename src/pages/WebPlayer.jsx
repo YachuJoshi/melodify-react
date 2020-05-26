7e8fe26a113e16ae46582ff18717c4566b942c59
Yachu Joshi <yachujoshi1234@gmail.com>
2020-05-27 00:04:56 +0545

    SideBar Appeneded :tada:

--- src/pages/WebPlayer.jsx
+++ src/pages/WebPlayer.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import Logo from '../img/melodify-logo.png';
 
+import { MdExplore } from 'react-icons/md';
+import {
+  IoMdVolumeHigh,
+  IoMdRadio,
+  IoMdMic,
+  IoIosMusicalNote,
+  IoMdAlbums,
+  IoMdHeart
+} from 'react-icons/io';
+import { GiCompactDisc } from 'react-icons/gi';
+import { FaFile } from 'react-icons/fa';
+
 import styles from '../sass/pages/webplayer.module.scss';
 
 const WebPlayerPage = () => {
@@ -8,6 +20,7 @@ const WebPlayerPage = () => {
     <>
       <div className={styles.container}>
         <header className={styles.header}></header>
+
         <div className={styles.sidebar}>
           <figure
             className={styles.logo}
@@ -22,7 +35,75 @@ const WebPlayerPage = () => {
               Melodify
             </figcaption>
           </figure>
-          
+
+          <div className={styles.recommend}>
+
+            <h3 className={styles.recommend__heading}>
+              Recommend
+            </h3>
+            <ul
+              className={styles.recommend__list}>
+
+              <li className={`${styles.recommend__item} ${styles.recommend__item__active}`}>
+                <MdExplore className={styles.recommend__icon} />
+                <p className={styles.recommend__text}>Explore</p>
+              </li>
+
+              <li className={styles.recommend__item}>
+                <IoMdVolumeHigh className={styles.recommend__icon} />
+                <p className={styles.recommend__text}>Genres</p>
+              </li>
+
+              <li className={styles.recommend__item}>
+                <IoMdRadio className={styles.recommend__icon} />
+                <p className={styles.recommend__text}>Radio</p>
+              </li>
+
+              <li className={styles.recommend__item}>
+                <IoMdMic className={styles.recommend__icon} />
+                <p className={styles.recommend__text}>Artists</p>
+              </li>
+
+              <li className={styles.recommend__item}>
+                <GiCompactDisc className={styles.recommend__icon} />
+                <p className={styles.recommend__text}>Albums</p>
+              </li>
+
+            </ul>
+          </div>
+
+          <div className={styles.library}>
+
+            <h3 className={styles.library__heading}>
+              My Library
+            </h3>
+            <ul
+              className={styles.library__list}>
+
+              <li className={styles.library__item}>
+                <IoIosMusicalNote className={styles.library__icon} />
+                <p className={styles.library__text}>Recently Played</p>
+              </li>
+
+              <li className={styles.library__item}>
+                <IoMdAlbums className={styles.library__icon} />
+                <p className={styles.library__text}>Albums</p>
+              </li>
+
+              <li className={styles.library__item}>
+                <IoMdHeart className={styles.library__icon} />
+                <p className={styles.library__text}>Favourite Songs</p>
+              </li>
+
+              <li className={styles.library__item}>
+                <FaFile className={styles.library__icon} />
+                <p className={styles.library__text}>Local Files</p>
+              </li>
+
+            </ul>
+          </div>
+
+
         </div>
         <div className={styles.weeklytracks}>WeeklyTracks</div>
         <div className={styles.topcharts}>Top Charts</div>
